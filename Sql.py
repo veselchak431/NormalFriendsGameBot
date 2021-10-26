0a6012abf0704146e56e054ac8ab88e4871b54dd
Your Name <krokik@mail.ru>
2021-10-26 13:29:22 +0300

    version with off webhook

--- Sql.py
+++ Sql.py
@@ -10,7 +10,9 @@ import os
 
 from flask import Flask, request
 
-server = Flask(__name__)
+#server = Flask(__name__)
+
+print("start on version 1.0")
 
 connect = sqlite3.connect('game.db')
 cursor = connect.cursor()
@@ -325,20 +327,21 @@ bot.add_custom_filter(BusyPleers())
 bot.add_custom_filter(EnableOneBiometry())
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+#@server.route('/' + TOKEN, methods=['POST'])
+#def getMessage():
+#    json_string = request.get_data().decode('utf-8')
+#    update = telebot.types.Update.de_json(json_string)
+#    bot.process_new_updates([update])
+#    return "!", 200
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://friendsbotgame.herokuapp.com/' + TOKEN)
-    return "!", 200
+#@server.route("/")
+#def webhook():
+#    bot.remove_webhook()
+#    bot.set_webhook(url='https://friendsbotgame.herokuapp.com/' + TOKEN)
+#    return "!", 200
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+#if __name__ == "__main__":
+#    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+bot.polling(none_stop=True, interval=0)
